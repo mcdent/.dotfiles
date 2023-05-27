e2c1e9ec070d2e4195de6e2869aebfb9acb75666
Mike <mcdent@gmail.com>
2023-05-27 10:59:32 +0100

    Update install.sh
    Some logic to check for an existing install os nix and allow user to choose to install etc.

--- install.sh
+++ install.sh
@@ -1,9 +1,38 @@
-# install nix
-# curl -L https://nixos.org/nix/install | sh
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix/tag/v0.9.0 | sh -s -- install
-# source nix
-# . ~/.nix-profile/etc/profile.d/nix.sh
-. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+#!/bin/bash
+# set locale, stops the warnings from perl.
+export LC_ALL="en_GB.UTF-8"
+export LANG="en_GB.UTF-8"
+export LANGUAGE="en_GB.UTF-8"
+# Check to see if nix is already installed.
+if [[ -f /nix/receipt.json || -d /nix ]]; then
+    echo "Looks like Nix is already installed."
+    read -p "Do you want to continue with the rest of the script? (Y/n): " decision
+    if [[ $decision != "Y" && $decision != "y" ]]; then
+        echo "Aborting."
+        exit 0
+    fi
+else
+    echo "Looks like Nix has not been installed."
+    read -p "Do you want to install Nix? (Y/n): " decision
+    if [[ $decision != "Y" && $decision != "y" ]]; then
+        echo "Aborting."
+        exit 0
+    fi
+
+    # Proceed with Nix installation
+    curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix/tag/v0.9.0 | sh -s -- install
+    # source nix
+    . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+fi
+
+read -p "Do you want to continue with package installation? (Y/n): " decision
+if [[ $decision != "Y" && $decision != "y" ]]; then
+    echo "Aborting."
+    exit 0
+fi
+# Continue with package installation here
+echo "Installing packages..."
+
 # install packages
 nix profile install \
     nixpkgs#zsh \
@@ -15,7 +44,7 @@ nix profile install \
      nixpkgs#yarn \
      nixpkgs#fzf \
      nixpkgs#ripgrep \
-     nixpkgs#bat \                                                                                     
+     nixpkgs#bat \
      nixpkgs#gnumake \
      nixpkgs#gcc \
      nixpkgs#direnv \
@@ -26,7 +55,7 @@ nix profile install \
      nixpkgs#nodejs-18_x \
      nixpkgs#nodePackages_latest.prettier \
      nixpkgs#shellcheck
-
+sleep 2
 # stow dotfiles
 stow git
 stow nvim
@@ -54,11 +83,11 @@ ln -sf $HOME/.dotfiles/bin $HOME/.bin
 # use zsh as default shell
 sudo chsh -s $(which zsh) $USER
 
-# bundle zsh plugins 
+# bundle zsh plugins
 antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
 
 # install neovim plugins
 nvim --headless +PlugInstall +qall
 
 # Use kitty terminal on MacOS
-[ `uname -s` = 'Darwin' ] nix profile install nixpkgs#kitty && stow kitty
+#[ `uname -s` = 'Darwin' ] nix profile install nixpkgs#kitty && stow kitty
