e59f001106b5803adcbe352a29dce9f152c79b76
Mike <mcdent@gmail.com>
2023-05-30 15:07:35 +0100

    Update backup.sh
    modified slightly to handle directories, we move the instead of making a copy.

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+# define the command. We use cp for a file and mv for directory
+cmd='cp'
 
 # Name of the original file
 origfile="$1"
 
+
 if [ -z "$origfile" ]; then
     echo "No filename provided. Usage: $0 filename"
     exit 1
-fi
 
-if [ ! -f "$origfile" ]; then
+elif [ -d "$origfile" ]; then
+    echo "$origfile is a directory, we will move instead of copy."
+    cmd='mv'
+
+elif [ ! -f "$origfile" ]; then
     echo "File $origfile does not exist."
     exit 1
 fi
@@ -22,6 +28,6 @@ while [ -f "${origfile}.MD.v${version}" ]; do
 done
 
 # Create a new backup
-cp "$origfile" "${origfile}.MD.v${version}"
+$cmd "$origfile" "${origfile}.MD.v${version}"
 
 echo "Backup ${origfile}.MD.v${version} created."
